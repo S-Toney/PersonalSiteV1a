4b8ad301e1a684aa4d2a67c7a84a96690b446704
Stacy Toney <stacy.toney@protonmail.com>
2020-11-28 18:58:09 -0600

    formatting added and beginning of lightbox

--- PersonalSiteV1a/js/custom.js
+++ PersonalSiteV1a/js/custom.js
@@ -48,3 +48,20 @@ function showGoogleMaps() {
 }
 
 //google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+
+///////////// Lightbox Code //////////////
+//  When a user clicks on a thumbnail image, display the lightbox container and insert an <img> tag, and allow user to clock off the lightbox
+$(".thumb").on("click", function () {
+
+    // display the lightbox container
+    $("#lightbox-container").fadeIn(500);
+
+    // insert an image tag
+    $("#lightbox-content").html('<img src="' + $(this).attr("src") + '" class="img-responsive" />');
+
+    // click off the lightbox
+    $("#lightbox-container").on("click", function () {
+        $(this).fadeOut(500);
+    });
+
+})
